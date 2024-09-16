31d2b285b2c219afd16d08ffbe7d0b1a7c1f3c84
xiebaoma <2402583447@qq.com>
2024-09-16 18:20:38 +0800

    用户下单，支付，催单

--- sky-take-out/sky-server/src/main/java/com/sky/controller/user/OrderController.java
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -43,12 +43,8 @@ public class OrderController {
     @ApiOperation("订单支付")
     public Result<OrderPaymentVO> payment(@RequestBody OrdersPaymentDTO ordersPaymentDTO) throws Exception {
         log.info("订单支付：{}", ordersPaymentDTO);
-//        OrderPaymentVO orderPaymentVO = orderService.payment(ordersPaymentDTO);
-//        log.info("生成预支付交易单：{}", orderPaymentVO);
-
-        Long orderId = orderService.paySuccess(ordersPaymentDTO.getOrderNumber());
-        OrderPaymentVO orderPaymentVO = new OrderPaymentVO();
-        orderPaymentVO.setOrderId(orderId);
+        OrderPaymentVO orderPaymentVO = orderService.payment(ordersPaymentDTO);
+        log.info("生成预支付交易单：{}", orderPaymentVO);
         return Result.success(orderPaymentVO);
     }
 
